13a679f2ede53e556f54f07d622b02c0e2a13000
Rational7 <129795244+Rational7@users.noreply.github.com>
2023-05-17 20:36:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//========================================================================================================================================================
+
+
+function changeSize() {
+  const input = document.getElementById('input').value;
+  document.getElementById('text').style.fontSize = input + "px";
+}
+
