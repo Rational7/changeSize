f093f8e990cf9f7a2064d23bd0058a0a8b8399f6
Rational7 <129795244+Rational7@users.noreply.github.com>
2023-05-21 08:11:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,6 @@
 function changeSize() {
   const input = document.getElementById('input').value;
   document.getElementById('text').style.fontSize = input + "px";
-}
-
+    const input2 = document.getElementById('input2').value;
+  document.getElementById('text').style.width = input2 + "px";
+};
